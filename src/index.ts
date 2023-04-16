b1854d752cb363c9670b0cd42d4d89bcb5089e36
LeeKyuSung <ks5050577@gmail.com>
2023-04-16 18:49:16 +0900

    final assignment setting

--- src/index.ts
+++ src/index.ts
@@ -3,35 +3,23 @@
 // init({ url: "http://localhost:3000", debug: true });
 // exit(1);
 
-// import head from "./lodash/head";
-// import hasIn from "./lodash/hasIn";
-// import isBoolean from "./lodash/isBoolean";
-// import toString from "./lodash/toString";
-// import split from "./lodash/split";
-// import hasPath from "./lodash/hasPath";
-// import filter from "./lodash/filter";
-// import every from "./lodash/every";
-// import map from "./lodash/map";
-
-import {
-  head,
-  //   hasIn,
-  //   isBoolean,
-  //   toString,
-  //   split,
-  //   hasPath,
-  //   filter,
-  //   every,
-  //   map,
-} from "lodash";
+import head from "./lodash/head";
+import hasIn from "./lodash/hasIn";
+import isBoolean from "./lodash/isBoolean";
+import toString from "./lodash/toString";
+import split from "./lodash/split";
+import hasPath from "./lodash/hasPath";
+import filter from "./lodash/filter";
+import every from "./lodash/every";
+import map from "./lodash/map";
 
 console.log(head([1, 2, 3]));
-// console.log(hasIn({ a: 1 }, "a"));
-// console.log(isBoolean(true));
-// console.log(toString(1));
-// console.log(split("a-b-c", "-"));
-// console.log(hasPath({ a: { b: 1 } }, "a.b"));
-// console.log(filter([1, 2, 3], (x: number) => x > 1));
-// console.log(every([true, 1, null, "yes"], Boolean));
-// console.log(every([true, 1], Boolean));
-// console.log(map([1, 2, 3], (x: number) => x * 2));
+console.log(hasIn({ a: 1 }, "a"));
+console.log(isBoolean(true));
+console.log(toString(1));
+console.log(split("a-b-c", "-"));
+console.log(hasPath({ a: { b: 1 } }, "a.b"));
+console.log(filter([1, 2, 3], (x: number) => x > 1));
+console.log(every([true, 1, null, "yes"], Boolean));
+console.log(every([true, 1], Boolean));
+console.log(map([1, 2, 3], (x: number) => x * 2));
---
+++ src/index2.ts
@@ -0,0 +1,16 @@
+// import { init, exit } from "./myPackage";
+
+// init({ url: "http://localhost:3000", debug: true });
+// exit(1);
+
+import {
+  head,
+  // hasIn,
+  // isBoolean,
+  // toString,
+  // split,
+  // hasPath,
+  // filter,
+  // every,
+  // map,
+} from "lodash";
