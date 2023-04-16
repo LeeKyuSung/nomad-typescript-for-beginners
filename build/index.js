e3311fd40a0aa43c55b7e443ecd843be87b33cb2
LeeKyuSung <ks5050577@gmail.com>
2023-04-16 17:46:06 +0900

    Update index.js

--- build/index.js
+++ build/index.js
@@ -1,1 +1,23 @@
-var hello = function () { return "hi"; };
+import { init, exit } from "./myPackage";
+init({ url: "http://localhost:3000", debug: true });
+exit(1);
+// head([1, 2, 3]);
+// hasIn({ a: { b: 2 } }, "a.b");
+// isBoolean(false);
+// toString(null);
+// split("a-b-c", "-");
+// hasPath({ a: { b: 2 } }, "a.b");
+// filter([1, 2, 3], Boolean);
+// every([true, 1, null, "yes"], Boolean);
+// map(["6", "8", "10"], (n) => parseInt(n, 10));
+// import {
+//   head,
+//   hasIn,
+//   isBoolean,
+//   toString,
+//   split,
+//   hasPath,
+//   filter,
+//   every,
+//   map,
+// } from "lodash";
