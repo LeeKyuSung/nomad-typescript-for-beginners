d949e5ab7b249ca646f2f7dcb99ea76762fb3877
LeeKyuSung <ks5050577@gmail.com>
2023-04-16 19:01:46 +0900

    final assignment done

--- src/index2.ts
+++ src/index2.ts
@@ -5,12 +5,23 @@
 
 import {
   head,
-  // hasIn,
-  // isBoolean,
-  // toString,
-  // split,
-  // hasPath,
-  // filter,
-  // every,
-  // map,
+  hasIn,
+  isBoolean,
+  toString,
+  split,
+  hasPath,
+  filter,
+  every,
+  map,
 } from "lodash";
+
+console.log(head([1, 2, 3]));
+console.log(hasIn({ a: 1 }, "a"));
+console.log(isBoolean(true));
+console.log(toString(1));
+console.log(split("a-b-c", "-"));
+console.log(hasPath({ a: { b: 1 } }, "a.b"));
+console.log(filter([1, 2, 3], (x: number) => x > 1));
+console.log(every([true, 1, null, "yes"], Boolean));
+console.log(every([true, 1], Boolean));
+console.log(map([1, 2, 3], (x: number) => x * 2));
